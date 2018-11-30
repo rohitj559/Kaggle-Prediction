d790dfff8edb3393618bbfe247aa007be5d5514f
Rohit Jagannath <rohitj559@gmail.com>
2018-11-30 16:00:20 -0600

    testing

--- kaggle_prediction_script.py
+++ kaggle_prediction_script.py
@@ -42,10 +42,12 @@ dataset_test['hour'] = dataset_test['datetime'].map(hour_of_day)
 X_test = dataset_test.iloc[:, [1,2,3,4,5,6,7,8,9]].values # array of independent features
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
-X_test = sc.transform(X_test)
+# =============================================================================
+# from sklearn.preprocessing import StandardScaler
+# sc = StandardScaler()
+# X = sc.fit_transform(X)
+# X_test = sc.transform(X_test)
+# =============================================================================
 
 ################################################################################
 # logistic regression
@@ -65,57 +67,63 @@ best_model = clf.fit(X, y)
 print('Best Penalty:', best_model.best_estimator_.get_params()['penalty'])
 print('Best C:', best_model.best_estimator_.get_params()['C'])
 # prediction on test data using the best model 
-y_pred = best_model.predict(X_test)
+# =============================================================================
+# y_pred = best_model.predict(X_test)
+# =============================================================================
 
 ################################################################################
 # random forest regression
 ################################################################################
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
-# prediction on test data using the best model 
-y_pred = regressor.predict(X_test)
-
-# Grid Search for Random Forest
-from sklearn.model_selection import GridSearchCV
-# Create the parameter grid based on the results of random search 
-param_grid = {
-    'bootstrap': [True],
-    'max_depth': [80, 90, 100, 110],
-    'max_features': [2, 3],
-    'min_samples_leaf': [3, 4, 5],
-    'min_samples_split': [8, 10, 12],
-    'n_estimators': [100, 200, 300, 1000]
-}
-# Create a based model
-rf = RandomForestRegressor()
-# Instantiate the grid search model
-grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
-                          cv = 3, n_jobs = -1, verbose = 2)
+# =============================================================================
+# from sklearn.ensemble import RandomForestRegressor
+# regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+# regressor.fit(X, y)
+# # prediction on test data using the best model 
+# y_pred = regressor.predict(X_test)
+# 
+# # Grid Search for Random Forest
+# from sklearn.model_selection import GridSearchCV
+# # Create the parameter grid based on the results of random search 
+# param_grid = {
+#     'bootstrap': [True],
+#     'max_depth': [80, 90, 100, 110],
+#     'max_features': [2, 3],
+#     'min_samples_leaf': [3, 4, 5],
+#     'min_samples_split': [8, 10, 12],
+#     'n_estimators': [100, 200, 300, 1000]
+# }
+# # Create a based model
+# rf = RandomForestRegressor()
+# # Instantiate the grid search model
+# grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
+#                           cv = 3, n_jobs = -1, verbose = 2)
+# =============================================================================
 ################################################################################
 # Simple Linear Regression
 ################################################################################
 # Fitting Simple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X, y)
-
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
-
-
-# results evaluation
-# estimate the rms for training prediction by comparing with ground truth information
-# loading the ground truth predictions
-dataset_ground_truth = pd.read_csv('sample_prediction.csv', header=0)
-y_ground_truth = dataset_ground_truth.iloc[:, 1].values
-rms_score = np.sqrt(mean_squared_error(y_ground_truth,y_pred))
-
-# generate output csv for kaggle prediction
-dataset_test['count'] = y_pred
-# save the predicted count as a csv with a header column and datetime row
-dataset_test_final = dataset_test[['datetime', 'count']].to_csv('my_prediction.csv',
-                                                index=False, header=True)
+# =============================================================================
+# from sklearn.linear_model import LinearRegression
+# regressor = LinearRegression()
+# regressor.fit(X, y)
+# 
+# # Predicting the Test set results
+# y_pred = regressor.predict(X_test)
+# 
+# 
+# # results evaluation
+# # estimate the rms for training prediction by comparing with ground truth information
+# # loading the ground truth predictions
+# dataset_ground_truth = pd.read_csv('sample_prediction.csv', header=0)
+# y_ground_truth = dataset_ground_truth.iloc[:, 1].values
+# rms_score = np.sqrt(mean_squared_error(y_ground_truth,y_pred))
+# 
+# # generate output csv for kaggle prediction
+# dataset_test['count'] = y_pred
+# # save the predicted count as a csv with a header column and datetime row
+# dataset_test_final = dataset_test[['datetime', 'count']].to_csv('my_prediction.csv',
+#                                                 index=False, header=True)
+# =============================================================================
 
 
 
